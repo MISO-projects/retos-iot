14e0a59d9b62bd5043a63082887f0d2533a34a77
Alejandro Peña <ja.penat1@uniandes.edu.co>
2026-03-06 20:03:20 -0500

    update services ips

--- reto-capa-aplicacion/IOTMonitoringServer/settings.py
+++ reto-capa-aplicacion/IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "18.208.152.142"]
+ALLOWED_HOSTS = ["localhost", "3.83.225.170"]
 
 
 # Application definition
@@ -96,7 +96,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "34.205.72.49",  # Dirección IP de la base de datos
+        "HOST": "54.224.49.59",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -156,7 +156,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "54.209.126.215"
+MQTT_HOST = "3.83.142.201"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
